8567e35d3d887b1826d42571a46355e8022cc063
Tiqur <trevorbrage02@gmail.com>
2024-06-26 02:40:02 -0700

    Update content using filter

--- dcgenerator/content_generators/example_generator.py
+++ dcgenerator/content_generators/example_generator.py
@@ -1,3 +1,9 @@
-from aqt.utils import showInfo
+import random
 
-showInfo("example generator")
+
+def generate_front():
+    return f"Front: {random.randint(1, 100)}"
+
+
+def generate_back():
+    return f"Back: {random.randint(1, 100)}"
--- dcgenerator/dcgenerator.py
+++ dcgenerator/dcgenerator.py
@@ -1,55 +1,65 @@
-import logging
-from aqt import gui_hooks
 from aqt.utils import showInfo
 import os
+from anki import hooks
+from anki.template import TemplateRenderContext
 
 addon_path = os.path.dirname(__file__)
 generators_path = os.path.join(addon_path, "content_generators")
-prefix = "dcg#"
-
-logging.basicConfig(level=logging.INFO)
+filter_name = "dcg"
 
 
-def run_script(path):
+def get_script_dict(path, exec_dict):
     try:
         with open(path, 'r', encoding='utf-8') as f:
             script_contents = f.read()
-            exec(script_contents)
+            exec(script_contents, exec_dict)
+
     except Exception as e:
-        logging.error(f"Error executing script {path}: {e}")
+        showInfo(f"Error executing script: {e}")
         raise
 
 
-def dynamic_content(card):
+def my_field_filter(field_text: str, field_name: str, filter_name: str, context: TemplateRenderContext) -> str:
+    if not filter_name.startswith(filter_name):
+        # not our filter, return string unchanged
+        return field_text
+
+    # split the name into the 'info' prefix, and the rest
     try:
-        # Get tags associated with the current card
-        tags = card.note().tags
+        (label, card_property, script_name) = filter_name.split("-", maxsplit=2)
+    except ValueError:
+        return invalid_name(filter_name)
 
-        showInfo(f"Tags for next card: {', '.join(tags)}")
+    # Get Generator file path
+    generator_file_path = os.path.join(generators_path, f"{script_name}.py")
 
-        for tag in tags:
-            if tag.startswith(prefix):
-                # Get tag without prefix
-                generator_file_path = os.path.join(
-                    generators_path,
-                    f"{tag[len(prefix):]}.py"
-                )
+    # Generate front and back
+    exec_dict = {}
+    get_script_dict(generator_file_path, exec_dict)
 
-                # Check if script exists
-                if os.path.exists(generator_file_path):
-                    showInfo(f"Executing script: {generator_file_path}")
+    if 'generate_front' not in exec_dict:
+        showInfo("Required method 'generate_front()' not found")
+        return
 
-                    # Attempt to execute script
-                    run_script(generator_file_path)
-                else:
-                    showInfo(f"Generation script {generator_file_path} does not exist for tag: {tag}")
+    if 'generate_back' not in exec_dict:
+        showInfo("Required method 'generate_back()' not found")
+        return
 
-            # Should only contain one dynamic tag
-            break
-    except Exception as e:
-        logging.error(f"Error processing dynamic content for card {card.id}: {e}")
+    front_text = exec_dict['generate_front']()
+    back_text = exec_dict['generate_back']()
+
+    # call the appropriate function
+    if card_property == "front":
+        return front_text
+    elif card_property == "back":
+        return back_text
+    else:
+        return invalid_name(filter_name)
+
+
+def invalid_name(filter_name: str) -> str:
+    return f"invalid filter name: {filter_name}"
 
 
 def main():
-    gui_hooks.reviewer_did_show_question.append(dynamic_content)
-
+    hooks.field_filter.append(my_field_filter)
